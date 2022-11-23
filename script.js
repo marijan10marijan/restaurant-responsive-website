ffd08a38ca2ce68988e283cafee6c56f7f0fa7fc
Marijan Marković <85864921+marijan10marijan@users.noreply.github.com>
2022-11-23 09:49:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,8 @@ const button = document.querySelector('.menu');
 let showLinks = document.querySelector('.links-box');
 
 
-function displayLinks() {
-    button.addEventListener('click', () => {
-        showLinks.classList.toggle('links-box-display');
-        if (showLinks.classList.contains('links-box-display')) {
-            button.style.color = 'black';
-        } else {
-            button.style.color = 'white';
-        }
-    })
-}
 
-
-displayLinks();
+button.addEventListener('click', () => {
+  button.classList.toggle('active');
+  showLinks.classList.toggle('links-box-display');
+})
